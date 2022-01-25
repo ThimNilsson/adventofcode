e2a14a04874cc6138dd227d282b459be22176a90
ThimNilsson <98350608+ThimNilsson@users.noreply.github.com>
2022-01-25 09:59:34 +0100

    Update inputdata.py
    added comment

--- day1/inputdata.py
+++ day1/inputdata.py
@@ -35,6 +35,7 @@ class InputData:
 
     def calc_num_of_increases(self):
         length = len(self.int_list)
+        # the first value is skipped, since there is no previous value to compare with
         for i in range (1, length,1):
             if self.int_list[i] > self.int_list[i-1]: 
                 self.num_of_increases += 1
