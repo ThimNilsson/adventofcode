be95538612e5801950a359662164459ae91b907c
Thim Nilsson <thim.nilsson@gmail.com>
2022-01-25 09:39:09 +0000

    added test for empty list. Added comments

--- day1/test.py
+++ day1/test.py
@@ -33,14 +33,21 @@ class TestHelpers(unittest.TestCase):
         with self.assertRaises(Exception):
             input_data.inputfile_lines_to_int_list()
 
+    def test_inputfile_lines_to_int_list_empty_input(self):
+        input_data = InputData("./test_files/empty_input.txt")
+        with self.assertRaises(Exception):
+            input_data.read_input()
+
     def test_calc_num_of_increases_0(self):
-        input_data = InputData("./test_files/invalid_input.txt")
+        input_data = InputData("./test_files/empty_input.txt")
+        # Mock the data
         input_data.int_list = [3, 2, 1]
         input_data.calc_num_of_increases()
         self.assertEqual(input_data.num_of_increases, 0)
 
     def test_calc_num_of_increases_2(self):
-        input_data = InputData("./test_files/invalid_input.txt")
+        input_data = InputData("./test_files/empty_input.txt")
+        # Mock the data
         input_data.int_list = [1, 2, 1, 3]
         input_data.calc_num_of_increases()
         self.assertEqual(input_data.num_of_increases, 2)
